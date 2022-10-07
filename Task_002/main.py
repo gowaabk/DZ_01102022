b4a54492b89b69034624714777e356c10aa5394e
gowaabk <107508551+gowaabk@users.noreply.github.com>
2022-10-07 23:05:17 +0300

    Update main.py

--- Task_002/main.py
+++ Task_002/main.py
@@ -4,7 +4,7 @@ from telegram.ext import Updater, CommandHandler
 from bot_commands import *
 import weather as wea
 
-TOKEN='5766062444:AAFio0sBZ20a8Z-7ATc67j_xG_BNi39OMH8'
+TOKEN='my_token'
 bot = telegram.Bot(token=TOKEN)
 
 #print(bot.get_me())
